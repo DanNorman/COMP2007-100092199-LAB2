e273a283e630606fdc2720c8ec4da9d9d8db0a68
DanNorman <Daniel.Norman@MyGeorgian.ca>
2016-05-26 11:28:45 -0400

    added navbar User Control

---
+++ Lab2/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab2
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ActiveNavBar();
+        }
+
+        /**
+         *This method adds the CSSclass of "active" to list items  
+         * 
+         * @method ActiveNavBar
+         * @return void
+         */
+        private void ActiveNavBar()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
